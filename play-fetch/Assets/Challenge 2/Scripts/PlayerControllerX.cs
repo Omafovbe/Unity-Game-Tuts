7662a55ee1895408982884b1a0edddb50b3a5c24
Omafovbe <i.omafovbe@gmail.com>
2021-09-21 02:19:16 +0100

    Play Fetch Challenge -Made balls spawn from the top of the screen -Made the player spawn dogs -Balls destroyed when they come in contact with the dog and also made the game objects destroy as they leave the screen -Made balls spawned randomly and for a bonus -BONUS!! Made the spawn interval for the balls randowm as well as the dogs spawned at a given time rate

---
+++ play-fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool spamCheck = true;
+    
+
+    void Start()
+    {
+        InvokeRepeating("SpamChecker", 0.5f, 0.7f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && spamCheck)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spamCheck = !spamCheck;
+        }
+    }
+
+    void SpamChecker()
+    {
+        spamCheck = !spamCheck;
+    }
+}
